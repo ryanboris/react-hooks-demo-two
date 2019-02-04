0c6f55c287800cd35ac96e528d86c56cf1c42b47
ryanboris <ryan.boris@gmail.com>
2019-02-04 06:49:18 -0500

    ✨ renders links on react hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [hits, setHits] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => setHits(response.data.hits))
+      .catch(e => console.error(e));
+  }, []);
+
   return (
     <>
-      <h2>Hey</h2>
+      <ol>
+        {hits.map(hit => (
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>
+        ))}
+      </ol>
     </>
   );
 }
